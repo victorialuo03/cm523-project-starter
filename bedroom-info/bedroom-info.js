b7eabc9b2aec1f22c53a70542bd718cbdb58a6ce
Victoria Luo <toriluo@bu.edu>
2023-05-02 09:45:07 -0400

    with bedroominfo

---
+++ bedroom-info/bedroom-info.js
@@ -0,0 +1,45 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+function changeContent() {
+    var button = document.getElementById("button1");
+    if (button.innerHTML === "Button 1") {
+      button.innerHTML = "Button 2";
+    } else {
+      button.innerHTML = "Button 1";
+    }
+  }
