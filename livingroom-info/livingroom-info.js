ded2fb603dddf2754e6489ec9470a093ee991e1c
Victoria Luo <toriluo@bu.edu>
2023-05-05 17:46:10 -0400

    final changes to js

--- livingroom-info/livingroom-info.js
+++ livingroom-info/livingroom-info.js
@@ -1,25 +1,31 @@
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
+let activeModal = null;
 
 openModalButtons.forEach(button => {
   button.addEventListener('click', () => {
     const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
+    if (activeModal !== null) {
+      closeModal(activeModal);
+    }
+    openModal(modal);
+    activeModal = modal;
   })
 })
 
 overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
+  if (activeModal !== null) {
+    closeModal(activeModal);
+    activeModal = null;
+  }
 })
 
 closeModalButtons.forEach(button => {
   button.addEventListener('click', () => {
     const modal = button.closest('.modal')
-    closeModal(modal)
+    closeModal(modal);
+    activeModal = null;
   })
 })
 
@@ -34,12 +40,3 @@ function closeModal(modal) {
   modal.classList.remove('active')
   overlay.classList.remove('active')
 }
-
-function changeContent() {
-    var button = document.getElementById("button1");
-    if (button.innerHTML === "Button 1") {
-      button.innerHTML = "Button 2";
-    } else {
-      button.innerHTML = "Button 1";
-    }
-  }
